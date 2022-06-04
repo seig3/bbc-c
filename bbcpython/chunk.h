63eaff300248bdef373b7ac8c14ea406979ebe9c
seig3 <106863298+seig3@users.noreply.github.com>
2022-06-04 21:37:37 +0900

    Update chunk.h

--- bbcpython/chunk.h
+++ bbcpython/chunk.h
@@ -32,6 +32,6 @@ typedef struct sChunk Chunk;
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte, unsigned int lineNum);
-int addConstant(Chunk* chunk, struct Value *value);
+int addConstant(Chunk* chunk, Value *value);
 
 #endif
