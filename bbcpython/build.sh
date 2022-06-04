07d133d7f96284c384ea573e42c7835466dda7e9
seig3 <106863298+seig3@users.noreply.github.com>
2022-06-04 21:22:16 +0900

    Update build.sh

--- bbcpython/build.sh
+++ bbcpython/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python3 ../main.py chunk.c compiler.c debug.c main.c memory.c object.c scanner.c value.c vm.c -static
+zcc +msx -subtype=msxdos2 -create-app -DAMALLOC chunk.c compiler.c debug.c main.c memory.c object.c scanner.c value.c vm.c -o pyz8n.com
