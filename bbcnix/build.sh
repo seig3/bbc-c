400719b7c452bcd8c1f6f8f2a5554ee00b9d3b13
seig3 <106863298+seig3@users.noreply.github.com>
2022-06-04 17:15:28 +0900

    Delete bbcnix directory

--- bbcnix/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-python3 ../main.py main.c && \
-python3 ../main.py io.s && \
-python3 ../main.py main.o io.o -static
--- bbcnix/io.h
+++
@@ -1,4 +0,0 @@
-#include "stdint.h"
-
-void writeio(uint16_t addr, uint8_t val);
-uint8_t readio(uint16_t addr);
