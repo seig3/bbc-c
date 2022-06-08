8cb3955d6c0b9c08c638e979c023eb3fe86cd385
seig3 <seig3@hotmail.com>
2022-06-08 22:08:10 +0900

    mv .img to .dsk

--- source/build.sh
+++ source/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 zcc +msx -subtype=msxdos2 -create-app -DAMALLOC chunk.c compiler.c debug.c main.c memory.c object.c scanner.c value.c vm.c -o pyz8n.com
+mv pyz8n.img pyz8n.dsk
